f21a72ab94ed8dc37a3d91de2c21925264ab784b
GGs-Leisure-Time <63327516+GGs-Leisure-Time@users.noreply.github.com>
2020-04-30 16:43:31 +0800

    Unity0428HW

---
+++ Assets/Level 1 Monster Pack/MONSTER1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class MONSTER1 : MonoBehaviour
+{
+    //prefab-->預製物:預先製作好的物品
+    [Header("HP"), Range(0, 200)]
+    public int hp;
+    [Header("Attack")]
+    public int atk;
+    [Header("HEAL")]
+    public int heal;
+
+
+
+
+
+}
---
+++ Assets/Level 1 Monster Pack/Practice.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class Practice : MonoBehaviour
+{
+
+
+
+    public MONSTER1 slime;
+    public MONSTER1 bat;
+
+
+
+    [Header("文字：輸出結果")]
+    public Text result;
+
+
+
+    public void Btatk()
+    {
+        //輸出結果.文字內容 =
+        result.text = "史萊姆受到蝙蝠" + bat.atk + "點的攻擊" +　
+            "\n"+ "史萊姆血量：" + (slime.hp -= bat.atk);
+    }
+
+    public void Btheal()
+    {
+        result.text = "蝙蝠受到" + bat.heal + "點的治癒" +
+            "\n" + "蝙蝠血量：" + (bat.hp += bat.heal);
+    }
+
+    public void Slatk()
+    {
+        result.text = "蝙蝠受到史萊姆" + slime.atk + "點的攻擊" +
+            "\n" + "蝙蝠血量：" + (bat.hp -= slime.atk);
+    }
+
+
+    public void Slheal()
+    {
+        result.text = "史萊姆受到" + slime.heal + "點的治癒" +
+            "\n" + "史萊姆血量：" + (slime.hp += slime.heal);
+    }
+
+
+
+}
+
+
+
+
+
